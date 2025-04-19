e902514bb2bfe0ebfbdae1959fa03bf26647bf46
binhltl <binhltl2001@gmail.com>
2025-04-19 11:41:57 +0700

    fix ssl

--- main.py
+++ main.py
@@ -31,6 +31,5 @@ app.include_router(router)
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, 
-                ssl_keyfile="key.pem", ssl_certfile="cert.pem")
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
     
